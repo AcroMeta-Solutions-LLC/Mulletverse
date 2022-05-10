c2153e4d70e124dfa362aa0e9435a855f423f2aa
Thales Ludwig <thales.ludwig@gmail.com>
2022-05-10 11:46:43 -0300

    Add Featured Artitsts spinner

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { Fragment, useEffect, useRef, useState } from "react";
 import Image from "next/image";
-import { NFT, INFTProps } from "web3uikit";
+import { NFT, INFTProps, Loading } from "web3uikit";
 import { useMoralis } from "react-moralis";
 import { toast } from "react-toastify";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
@@ -23,11 +23,13 @@ import {
   Chevron,
   SliderWrapper,
   ButtonNFTBuy,
+  LoadingWrapper,
 } from "../styles/HomeStyled";
 
 const Home: NextPage = () => {
   const { isInitialized, Moralis } = useMoralis();
   const [nfts, setNfts] = useState<INFTProps[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const sliderRef = useRef<HTMLDivElement>(null);
 
   const ADDRESS: string = "0xd45058Bf25BBD8F586124C479D384c8C708CE23A";
@@ -35,6 +37,7 @@ const Home: NextPage = () => {
 
   useEffect(() => {
     if (isInitialized) {
+      setIsLoading(true);
       Moralis.Web3API.account
         .getNFTs({ address: ADDRESS, chain: CHAIN, limit: 20 })
         .then((response) => {
@@ -51,6 +54,9 @@ const Home: NextPage = () => {
         })
         .catch((error) => {
           toast.error(error);
+        })
+        .finally(() => {
+          setIsLoading(false);
         });
     }
   }, [isInitialized, Moralis.Web3API.account]);
@@ -95,22 +101,33 @@ const Home: NextPage = () => {
       </About>
       <Featured>
         <FeaturedTitle>Featured Artists</FeaturedTitle>
-        <SliderWrapper>
-          <FeaturedSlider ref={sliderRef}>
-            {nfts.map((nft) => (
-              <NFTWrapper key={nft.tokenId}>
-                <NFT {...nft} />
-                <ButtonNFTBuy>BUY</ButtonNFTBuy>
-              </NFTWrapper>
-            ))}
-          </FeaturedSlider>
-          <Chevron position="left" onClick={() => scrollSlider("left")}>
-            <FiChevronLeft size={30} />
-          </Chevron>
-          <Chevron position="right" onClick={() => scrollSlider("right")}>
-            <FiChevronRight size={30} />
-          </Chevron>
-        </SliderWrapper>
+        {isLoading && (
+          <LoadingWrapper>
+            <Loading />
+          </LoadingWrapper>
+        )}
+        {!isLoading && (
+          <SliderWrapper>
+            <FeaturedSlider ref={sliderRef}>
+              {nfts.map((nft) => (
+                <NFTWrapper key={nft.tokenId}>
+                  <NFT {...nft} />
+                  <ButtonNFTBuy>BUY</ButtonNFTBuy>
+                </NFTWrapper>
+              ))}
+            </FeaturedSlider>
+            {nfts.length > 0 && (
+              <Fragment>
+                <Chevron position="left" onClick={() => scrollSlider("left")}>
+                  <FiChevronLeft size={30} />
+                </Chevron>
+                <Chevron position="right" onClick={() => scrollSlider("right")}>
+                  <FiChevronRight size={30} />
+                </Chevron>
+              </Fragment>
+            )}
+          </SliderWrapper>
+        )}
       </Featured>
     </Fragment>
   );

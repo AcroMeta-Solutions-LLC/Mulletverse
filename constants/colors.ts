c0cc44c3f963322ceaf588e72c3d8c52fe4c448c
Thales Ludwig <thales.ludwig@gmail.com>
2022-05-25 16:00:25 -0300

    FEATURE - Income Tracker

--- constants/colors.ts
+++ constants/colors.ts
@@ -3,11 +3,13 @@ export const COLORS = {
   CLEAR: "#f2f6ff",
   DARK: "#4e5760",
   PURPLE: {
-    LIGHT: "#c77fcd",
-    DARK: "#6c4cd8",
+    LIGHT: "#a57bed",
+    DARK: "#7f4cd8",
   },
   WHITE: "#ffffff",
   BLUE: "#448bb7",
 };
+
+export const CHART_COLORS = ["#8d5697", "#d6e6f6", "#b2dafb", "#ede8eb", "#dcb4b3"];
 
 export default COLORS;
--- styles/DashboardStyled.ts
+++ styles/DashboardStyled.ts
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 import { SCREEN } from "../constants/screen";
 import { TYPOGRAPHY } from "../constants/typography";
+import ThemeType from "../types/themeType";
 
 export const Main = styled.main`
-  padding: 70px 0 50px 0;
+  padding: 50px 0;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
   min-height: calc(100vh - 72px);
 
   @media only screen and (max-width: ${SCREEN.TABLET_SMALL}) {
@@ -13,14 +15,59 @@ export const Main = styled.main`
   }
 `;
 
-export const Container = styled.div`
+export const Section = styled.section`
   max-width: ${SCREEN.DESKTOP};
   display: flex;
   flex-direction: column;
+  width: 100%;
 `;
 
-export const Title = styled.h1`
+export const Title = styled.h2`
   font-size: ${TYPOGRAPHY.SIZE.HEADLINE_1};
   font-weight: ${TYPOGRAPHY.WEIGHT.HEADLINE_1};
   align-self: center;
+  margin: 45px 0;
+`;
+
+export const IncomeTracker = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+
+  @media only screen and (max-width: ${SCREEN.TABLET_SMALL}) {
+    flex-direction: column;
+  }
+`;
+
+export const DataArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  @media only screen and (max-width: ${SCREEN.TABLET_SMALL}) {
+    margin-bottom: 50px;
+  }
+`;
+
+export const DataLabel = styled.span`
+  color: ${({ theme }: { theme: ThemeType }) => theme.TITLE};
+  font-weight: ${TYPOGRAPHY.WEIGHT.SUBTITLE_1};
+  font-size: ${TYPOGRAPHY.SIZE.SUBTITLE_1};
+`;
+
+export const Data = styled.div`
+  color: ${({ theme, hasColor }: { theme: ThemeType; hasColor: boolean }) => (hasColor ? "green" : theme.TEXT)};
+  font-weight: ${TYPOGRAPHY.WEIGHT.SUBTITLE_2};
+  font-size: ${TYPOGRAPHY.SIZE.SUBTITLE_2};
+  margin-bottom: 30px;
+`;
+
+export const ChartArea = styled.div`
+  width: 500px;
+  height: 300px;
+
+  @media only screen and (max-width: ${SCREEN.TABLET_SMALL}) {
+    width: 100%;
+  }
 `;

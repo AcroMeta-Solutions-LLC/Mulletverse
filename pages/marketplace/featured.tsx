a01f7180e2f7e6a961a97ba2db2565c5e7186fd8
Thales Ludwig <thales.ludwig@gmail.com>
2022-07-04 16:00:45 -0300

    Change the marketplace default chain to matic

--- pages/marketplace/featured.tsx
+++ pages/marketplace/featured.tsx
@@ -42,7 +42,7 @@ const Marketplace: NextPage = () => {
   const dispatch = useDispatch<AppDispatch>();
   const { pathname } = useRouter();
   const PAGE_SIZE = 20;
-  const [chain, setChain] = useState<ChainType>("eth");
+  const [chain, setChain] = useState<ChainType>("matic");
   const [isFilterOpen, setIsFilterOpen] = useState(false);
   const theme: any = useTheme();
   const { data, isLoading, total, nextCursor, previousCursor, page, hasError } = useSelector(
